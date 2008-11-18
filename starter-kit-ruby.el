9c62e54ef87cf158a4cb650f15700ac333fd4f7e
Phil Hagelberg <technomancy@gmail.com>
2008-11-18 15:32:14 -0800

    fix ruby binding

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -22,7 +22,7 @@
       (rename-buffer (format "*%s*" impl) t))))
 
 (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
-(define-key ruby-mode-map (kbd "RET") 'ruby-reindent-then-newline-and-indent)
+(define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
 (define-key ruby-mode-map (kbd "C-c l") (lambda ()
                                           (interactive) (insert "lambda")))
 
