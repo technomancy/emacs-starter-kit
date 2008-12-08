b4aa1147acc42741235e855bb8f674783d6422f9
Phil Hagelberg <technomancy@gmail.com>
2008-12-08 11:45:53 -0800

    Added some rake functions.

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -17,8 +17,34 @@
 
 (global-set-key (kbd "C-h r") 'ri)
 
+;; Rake files are ruby, too.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
+
+(add-hook 'ruby-mode-hook 'my-coding-hook)
+
+;;; Rake
+
+(defun pcomplete/rake ()
+  "Completion rules for the `ssh' command."
+  (pcomplete-here (pcmpl-rake-tasks)))
+
+(defun pcmpl-rake-tasks ()
+   "Return a list of all the rake tasks defined in the current
+projects.  I know this is a hack to put all the logic in the
+exec-to-string command, but it works and seems fast"
+   (delq nil (mapcar '(lambda(line)
+			(if (string-match "rake \\([^ ]+\\)" line) (match-string 1 line)))
+		     (split-string (shell-command-to-string "rake -T") "[\n]"))))
+
+(defun rake (task)
+  (interactive (list (completing-read "Rake (default: default): "
+                                      (pcmpl-rake-tasks))))
+  (shell-command-to-string (concat "rake " (if (= 0 (length task)) "default" task))))
+
 
 ;; Clear the compilation buffer between test runs.
 (eval-after-load 'ruby-compilation
@@ -29,8 +55,6 @@
            (with-current-buffer comp-buffer-name
              (delete-region (point-min) (point-max))))))
      (ad-activate 'ruby-do-run-w/compilation)))
-
-(add-hook 'ruby-mode-hook 'my-coding-hook)
 
 ;; TODO: set up ri
 ;; TODO: electric
