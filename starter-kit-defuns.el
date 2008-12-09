6ebc4e86ebf004471b829801c8a08ae441cad313
Phil Hagelberg <technomancy@gmail.com>
2008-12-09 11:42:35 -0800

    Make regen-autoloads backwards-compatible with 22.

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -109,11 +109,10 @@
 
 (defun regen-autoloads (&optional force-regen)
   "Regenerate the autoload definitions file if necessary and load it."
-  (interactive "p")
-  (setq reg force-regen)
+  (interactive "P")
   (let ((autoload-dir (concat dotfiles-dir "/elpa-to-submit"))
         (generated-autoload-file autoload-file))
-    (when (or (> force-regen 1) ;; prefix arg used
+    (when (or force-regen
               (not (file-exists-p autoload-file))
               (some (lambda (f) (file-newer-than-file-p f autoload-file))
                     (directory-files autoload-dir t "\\.el$")))
