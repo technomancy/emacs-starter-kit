443db1a122b7a97f68f1beb92d0909a855edc726
Phil Hagelberg <technomancy@gmail.com>
2008-12-15 15:43:27 -0800

    Set up flymake for ruby files.

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -50,9 +50,37 @@ exec-to-string command, but it works and seems fast"
 
 (add-hook 'ruby-mode-hook 'coding-hook)
 
+;;; Flymake
+
+(require 'flymake)
+
+;; Invoke ruby with '-c' to get syntax checking
+(defun flymake-ruby-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
+      flymake-err-line-patterns)
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (when (and buffer-file-name
+                       (file-writable-p
+                        (file-name-directory buffer-file-name))
+                       (file-writable-p buffer-file-name))
+              (local-set-key (kbd "C-c d")
+                             'flymake-display-err-menu-for-current-line)
+              (flymake-mode t))))
+
 ;; TODO: set up ri
 ;; TODO: electric
-;; TODO: flymake
 
 (provide 'starter-kit-ruby)
 ;; starter-kit-ruby.el ends here
