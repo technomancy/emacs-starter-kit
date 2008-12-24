9eedd2e7c407953567ac3085f48895bf26967a8b
Phil Hagelberg <technomancy@gmail.com>
2008-12-24 08:41:28 -0800

    Gemspecs are ruby code.

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -12,9 +12,10 @@
 
 (global-set-key (kbd "C-h r") 'ri)
 
-;; Rake files are ruby, too.
+;; Rake files are ruby, too, as are gemspecs.
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
