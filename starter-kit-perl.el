38dc7641a7b97b5ead421abca5474c3bac711c8d
John SJ Anderson <genehack@genehack.org>
2008-12-27 00:36:00 -0500

    starter-kit-perl added

---
+++ starter-kit-perl.el
@@ -0,0 +1,23 @@
+;;; starter-kit-perl.el --- Some helpful Perl code
+;;
+;; Part of the Emacs Starter Kit
+
+(eval-after-load 'cperl-mode
+  '(progn
+     (define-key cperl-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key cperl-mode-map (kbd "C-M-h") 'backward-kill-word)))
+
+(global-set-key (kbd "C-h P") 'perldoc)
+
+(add-to-list 'auto-mode-alist '("\\.(?:cgi|p[lm])$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
+(add-to-list 'auto-mode-alist '("\\.tt$" . tt-mode))
+
+;; We never want to edit Rubinius bytecode
+(add-to-list 'completion-ignored-extensions ".rbc")
+
+;; TODO: flymake
+;; TODO: electric bugaloo 
+
+(provide 'starter-kit-perl)
+;; starter-kit-perl.el ends here
