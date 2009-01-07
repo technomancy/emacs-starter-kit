8fb846f09cdc851fc0871505ca441473fb5ccf50
Phil Hagelberg <technomancy@gmail.com>
2009-01-07 11:30:50 -0800

    Add user-specific-dir to loadpath and load its contents. Thanks Seth!

--- init.el
+++ init.el
@@ -64,9 +64,14 @@
 ;; You can keep system- or user-specific customizations here:
 
 (setq system-specific-config (concat dotfiles-dir system-name ".el")
-      user-specific-config (concat dotfiles-dir user-login-name ".el"))
+      user-specific-config (concat dotfiles-dir user-login-name ".el")
+      user-specific-dir (concat dotfiles-dir user-login-name))
 
 (if (file-exists-p system-specific-config) (load system-specific-config))
 (if (file-exists-p user-specific-config) (load user-specific-config))
+
+(when (file-exists-p user-specific-dir)
+  (add-to-list 'load-path user-specific-dir)
+  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
 ;;; init.el ends here
