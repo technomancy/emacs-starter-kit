83d2e9b93050545def588eae0f55727dab28d7a5
Phil Hagelberg <technomancy@gmail.com>
2009-01-28 15:58:44 -0800

    Indent JS code correctly.

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -20,15 +20,59 @@
      (defun js-lambda () (interactive) (insert "function () {\n}")
        (backward-char 5))
 
+     (add-hook 'js2-mode-hook 'coding-hook)
+
      (define-key js2-mode-map (kbd "C-c l") 'js-lambda)
      (define-key js2-mode-map "\C-\M-h" 'backward-kill-word)
+
+     ;; Fix js2's crazy indentation
      (define-key js2-mode-map (kbd "TAB") (lambda () (interactive)
                                             (indent-for-tab-command)
                                             (back-to-indentation)))
 
-     (add-hook 'js2-mode-hook 'coding-hook)
      (setq js2-bounce-indent-flag nil
-           js2-indent-on-enter-key t)))
+           js2-indent-on-enter-key t)
+
+     (defun js-continued-var-decl-list-p ()
+       "Return non-nil if point is inside a continued variable declaration list."
+       (interactive)
+       (let ((start (save-excursion (js-re-search-backward "\\<var\\>" nil t))))
+         (and start
+              (save-excursion (re-search-backward "\n" start t))
+              (not (save-excursion
+                     (js-re-search-backward
+                      ";\\|[^, \t][ \t]*\\(/[/*]\\|$\\)" start t))))))
+     
+     (defun js-proper-indentation (parse-status)
+       "Return the proper indentation for the current line."
+       (save-excursion
+         (back-to-indentation)
+         (let ((ctrl-stmt-indent (js-ctrl-statement-indentation))
+               (same-indent-p (looking-at "[]})]\\|\\<case\\>\\|\\<default\\>"))
+               (continued-expr-p (js-continued-expression-p)))
+           (cond (ctrl-stmt-indent)
+                 ((js-continued-var-decl-list-p)
+                  (js-re-search-backward "\\<var\\>" nil t)
+                  (+ (current-indentation) js2-basic-offset))
+                 ((nth 1 parse-status)
+                  (goto-char (nth 1 parse-status))
+                  (if (looking-at "[({[][ \t]*\\(/[/*]\\|$\\)")
+                      (progn
+                        (skip-syntax-backward " ")
+                        (when (= (char-before) ?\)) (backward-list))
+                        (back-to-indentation)
+                        (cond (same-indent-p
+                               (current-column))
+                              (continued-expr-p
+                               (+ (current-column) (* 2 js2-basic-offset)))
+                              (t
+                               (+ (current-column) js2-basic-offset))))
+                    (unless same-indent-p
+                      (forward-char)
+                      (skip-chars-forward " \t"))
+                    (current-column)))
+                 (continued-expr-p js2-basic-offset)
+                 (t 0)))))))
 
 (defun esk-pp-json ()
   "Pretty-print the json object following point."
