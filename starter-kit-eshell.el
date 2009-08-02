07bde5da038624998ad9b9f6b33e195cd23fe58a
Eric Schulte <schulte.eric@gmail.com>
2009-08-02 17:20:04 -0600

    removing files

--- starter-kit-eshell.el
+++
@@ -1,8 +0,0 @@
-;;; starter-kit-eshell.el --- Making the defaults a bit saner
-;;
-;; Part of the Emacs Starter Kit
-
-
-
-(provide 'starter-kit-eshell)
-;;; starter-kit-eshell.el ends here
--- starter-kit-misc.el
+++
@@ -1,7 +0,0 @@
-;;; starter-kit-misc.el --- Things that don't fit anywhere else
-;;
-;; Part of the Emacs Starter Kit
-
-
-(provide 'starter-kit-misc)
-;;; starter-kit-misc.el ends here
