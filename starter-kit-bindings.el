d0e903c6aad90f360a1f63f8a7bc59fb36a2540a
gary <ge.iams@gmail.com>
2009-08-06 14:10:15 -0400

    no gnus, no jabber, thanks.

--- starter-kit-bindings.el
+++ starter-kit-bindings.el
@@ -71,14 +71,6 @@
 (global-set-key (kbd "C-c e") 'eval-and-replace)
 
 ;; Applications
-
-(global-set-key (kbd "C-c j") (lambda () (interactive) (switch-or-start 'jabber-connect "*-jabber-*")))
-(global-set-key (kbd "C-c g") (lambda () (interactive) (switch-or-start 'gnus "*Group*")))
-(global-set-key (kbd "C-c i") (lambda () (interactive) (switch-or-start (lambda ()
-                                                                     (rcirc-connect "irc.freenode.net"))
-                                                                   "*irc.freenode.net*")))
-(global-set-key (kbd "C-c J") 'jabber-send-presence)
-(global-set-key (kbd "C-c M-j") 'jabber-disconnect)
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; This is a little hacky since VC doesn't support git add internally
