b4c2c72f8b54ee4ba8353888682afb49119e9838
gary <ge.iams@gmail.com>
2009-08-07 14:41:11 -0400

    using osx's default browser and enabling delete-selection-mode

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -23,14 +23,10 @@
       save-place-file (concat dotfiles-dir "places"))
 
 ;; Set this to whatever browser you use
-;; (setq browse-url-browser-function 'browse-url-firefox)
-;; (setq browse-url-browser-function 'browse-default-macosx-browser)
-;; (setq browse-url-browser-function 'browse-default-windows-browser)
-;; (setq browse-url-browser-function 'browse-default-kde)
-;; (setq browse-url-browser-function 'browse-default-epiphany)
-;; (setq browse-url-browser-function 'browse-default-w3m)
-;; (setq browse-url-browser-function 'browse-url-generic
-;;       browse-url-generic-program "~/src/conkeror/conkeror")
+(setq browse-url-browser-function 'browse-default-macosx-browser)
+
+;; Replace the contents of a selection with typed text
+(delete-selection-mode t)
 
 ;; Transparently open compressed files
 (auto-compression-mode t)
