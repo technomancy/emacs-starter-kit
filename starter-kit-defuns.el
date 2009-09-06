bc907d277113d771fff2d3647c1724a0d2aaa37e
Phil Hagelberg <technomancy@gmail.com>
2009-09-06 13:21:08 -0700

    Add org bindings; fix clojure-project root heuristic.

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -86,6 +86,9 @@ Symbols matching the text at point are put first in the completion list."
 (defun turn-on-whitespace ()
   (whitespace-mode t))
 
+(defun turn-on-paredit ()
+  (paredit-mode t))
+
 (defun turn-off-tool-bar ()
   (tool-bar-mode -1))
 
