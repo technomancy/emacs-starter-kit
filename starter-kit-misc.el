0e17b376c20fd63c77c284191b72e0364cfb2e88
Phil Hagelberg <technomancy@gmail.com>
2009-10-13 16:58:26 -0700

    Only turn on mouse-wheel-mode if a window system is running.

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -5,11 +5,11 @@
 (when window-system
   (setq frame-title-format '(buffer-file-name "%f" ("%b")))
   (tooltip-mode -1)
+  (mouse-wheel-mode t)
   (blink-cursor-mode -1))
 
 (add-hook 'before-make-frame-hook 'turn-off-tool-bar)
 
-(mouse-wheel-mode t)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
