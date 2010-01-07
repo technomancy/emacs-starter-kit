c0b7ae5fd87675e896242557846c144cb55d274a
Phil Hagelberg <technomancy@gmail.com>
2010-01-06 16:09:47 -0800

    Add watchwords to coding hook.

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -11,14 +11,14 @@
 (add-hook 'espresso-mode-hook 'idle-highlight)
 (setq espresso-indent-level 2)
 
+;; If you prefer js2-mode, use this instead:
+;; (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+
 (eval-after-load 'espresso
   '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
           (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
           ;; fixes problem with pretty function font-lock
           (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-          (font-lock-add-keywords 'espresso-mode
-                        '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
-                           1 font-lock-warning-face t)))
           (font-lock-add-keywords
            'espresso-mode `(("\\(function *\\)("
                              (0 (progn (compose-region (match-beginning 1)
