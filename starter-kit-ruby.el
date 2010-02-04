ba3277e3e2f3d0b2a018bdecd6ea84a5c809e12c
gary <ge.iams@gmail.com>
2010-02-04 02:36:36 -0500

    registered bundler's manifest files with ruby-mode

--- starter-kit-ruby.el
+++ starter-kit-ruby.el
@@ -18,6 +18,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
