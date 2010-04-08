5f1214a3ebe38ad0748d697fec29d54e37226211
Phil Hagelberg <technomancy@gmail.com>
2010-04-08 09:35:57 -0700

    Cleanup IRC stuff.

--- phil/my-irc.el
+++ phil/my-irc.el
@@ -11,13 +11,10 @@
            '(("freenode.net" "#emacs" "#clojure" "#seattle.rb"
               "#leiningen" "#jclouds"))
            erc-prompt-for-nickserv-password nil)
-     (require 'erc-highlight-nicknames)
-     ;; (require 'erc-scrolltobottom)
      (require 'erc-services)
      (require 'erc-spelling)
      (erc-services-mode 1)
      (add-to-list 'erc-modules 'highlight-nicknames 'spelling)
-     ;; (add-to-list 'erc-modules 'scrolltobottom)
      (add-hook 'erc-connect-pre-hook (lambda (x) (erc-update-modules)))
      (set-face-foreground 'erc-input-face "dim gray")
      (set-face-foreground 'erc-my-nick-face "blue")
