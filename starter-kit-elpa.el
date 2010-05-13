0fbf41ffe0442d9669d393631f7adbfe8bcc8778
Phil Hagelberg <technomancy@gmail.com>
2010-05-12 19:03:43 -0700

    Add autoloads to work around bad yaml-mode packaging.

--- starter-kit-elpa.el
+++ starter-kit-elpa.el
@@ -43,6 +43,9 @@ just have to assume it's online."
 
 ;; Workaround for an ELPA bug that people are reporting but I've been
 ;; unable to reproduce:
-(autoload 'paredit-mode "paredit")
+(autoload 'paredit-mode "paredit" "" t)
+
+;; Workaround for bug in the ELPA package for yaml-mode
+(autoload 'yaml-mode "yaml-mode" "" t)
 
 (provide 'starter-kit-elpa)
