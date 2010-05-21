fbbb9e341b17069475615fcb9635f4b9ef4cd7b0
Phil Hagelberg <technomancy@gmail.com>
2010-05-21 13:27:43 -1000

    Render clojure's fn as ƒ.

--- starter-kit-lisp.el
+++ starter-kit-lisp.el
@@ -56,5 +56,12 @@
   (add-hook
    (intern (concat (symbol-name x) "-mode-hook")) 'run-coding-hook))
 
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(?\\(fn\\>\\)"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
 (provide 'starter-kit-lisp)
 ;; starter-kit-lisp.el ends here
