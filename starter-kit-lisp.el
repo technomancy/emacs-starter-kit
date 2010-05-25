fd24e101c4952bb3534844a7f7eb7aed181a7192
Phil Hagelberg <technomancy@gmail.com>
2010-05-24 18:25:19 -1000

    Make clojure's pretty-fns more discerning.

--- starter-kit-lisp.el
+++ starter-kit-lisp.el
@@ -58,7 +58,7 @@
 
 (eval-after-load 'clojure-mode
   '(font-lock-add-keywords
-    'clojure-mode `(("(?\\(fn\\>\\)"
+    'clojure-mode `(("(\\(fn\\>\\)"
                      (0 (progn (compose-region (match-beginning 1)
                                                (match-end 1) "ƒ")
                                nil))))))
