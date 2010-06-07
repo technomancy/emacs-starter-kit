0b055d74cd9c8bcd600e116be6b7f432bc1509d0
gary <ge.iams@gmail.com>
2010-06-06 23:25:30 -0400

    restored the starter-kit's miscellaneous setups, moving all of my  customizations to where the README stated they should be

---
+++ gary/misc.el
@@ -0,0 +1,68 @@
+(ansi-color-for-comint-mode-on)
+
+;; Enable select, disabled features
+(put 'narrow-to-region 'disabled nil)   ; narrow enabled
+(put 'narrow-to-page 'disabled nil)
+(put 'upcase-region 'disabled nil)      ; change case enabled
+(put 'eval-expression 'disabled nil)    ; allow eval commands
+
+(setq yank-pop-change-selection t
+      ffap-require-prefix t
+      skeleton-pair t
+      auto-save-default nil
+      minibuffer-max-depth nil
+      uniquify-buffer-name-style 'reverse
+      uniquify-separator "|"
+      uniquify-after-kill-buffer-p t
+      uniquify-ignore-buffers-re "^\\*")
+
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
+;; Replace the contents of a selection with typed text
+(delete-selection-mode t)
+
+;; Incremental minibuffer completion
+(icomplete-mode 1)
+
+(when (> emacs-major-version 21)
+  (ido-everywhere t)
+  (setq ido-confirm-unique-completion t
+        ido-default-buffer-method 'other-window
+        ido-use-filename-at-point t))
+
+(setq smex-prompt-string "smex ")
+(setq smex-save-file (concat dotfiles-dir "smex.save"))
+
+;; YASnippet
+(yas/initialize)
+(yas/load-directory (concat dotfiles-dir "snippets"))
+
+;; Yeah, Hippie expand
+(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
+
+;; More precise temporary file management
+(setq version-control t
+      kept-new-versions 16
+      kept-old-versions 2
+      delete-old-versions t
+      backup-by-copying-when-linked t)
+
+(add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode))
+(add-to-list 'auto-mode-alist '("\\.scpt$"        . applescript-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
+
+;; Keep and trash selected buffers
+(midnight-delay-set 'midnight-delay 4400)
+(add-to-list 'clean-buffer-list-kill-never-buffer-names "*msg*")
+(add-to-list 'clean-buffer-list-kill-never-regexps "^\\*shell-")
+(append clean-buffer-list-kill-buffer-names '("*Shell Command Output*"
+                                              "*Completions*"
+                                              "*Occur*"
+                                              "*Bookmark List*"
+                                              "*Ediff Registry*"
+                                              "*ack*"
+                                              "ri `"
+                                              "*markdown-output*"))
+(add-to-list 'clean-buffer-list-kill-regexps "\\.rb$")
+(add-to-list 'clean-buffer-list-kill-regexps "\\.el$")
--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -2,61 +2,71 @@
 ;;
 ;; Part of the Emacs Starter Kit
 
-(mouse-wheel-mode t)
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (tooltip-mode -1)
+  (mouse-wheel-mode t)
+  (blink-cursor-mode -1))
+
+(add-hook 'before-make-frame-hook 'turn-off-tool-bar)
+
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+(ansi-color-for-comint-mode-on)
 
-;; Enable select, disabled features
-(put 'narrow-to-region 'disabled nil)   ; narrow enabled
-(put 'narrow-to-page 'disabled nil)
-(put 'upcase-region 'disabled nil)      ; change case enabled
-(put 'eval-expression 'disabled nil)    ; allow eval commands
-
-(setq echo-keystrokes 0.1
+(setq visible-bell t
+      echo-keystrokes 0.1
+      font-lock-maximum-decoration t
+      inhibit-startup-message t
+      transient-mark-mode t
+      color-theme-is-global t
       shift-select-mode nil
-      yank-pop-change-selection t
-      ffap-require-prefix t
-      skeleton-pair t
+      mouse-yank-at-point t
       require-final-newline t
-      auto-save-default nil
-      minibuffer-max-depth nil
-      uniquify-buffer-name-style 'reverse
-      uniquify-separator "|"
-      uniquify-after-kill-buffer-p t
-      uniquify-ignore-buffers-re "^\\*"
+      truncate-partial-width-windows nil
+      uniquify-buffer-name-style 'forward
+      whitespace-style '(trailing lines space-before-tab
+                                  indentation space-after-tab)
+      whitespace-line-column 100
       ediff-window-setup-function 'ediff-setup-windows-plain
       oddmuse-directory (concat dotfiles-dir "oddmuse")
       xterm-mouse-mode t
       save-place-file (concat dotfiles-dir "places"))
 
 ;; Set this to whatever browser you use
-(setq browse-url-browser-function 'browse-url-default-macosx-browser)
-
-;; Replace the contents of a selection with typed text
-(delete-selection-mode t)
+;; (setq browse-url-browser-function 'browse-url-firefox)
+;; (setq browse-url-browser-function 'browse-default-macosx-browser)
+;; (setq browse-url-browser-function 'browse-default-windows-browser)
+;; (setq browse-url-browser-function 'browse-default-kde)
+;; (setq browse-url-browser-function 'browse-default-epiphany)
+;; (setq browse-url-browser-function 'browse-default-w3m)
+;; (setq browse-url-browser-function 'browse-url-generic
+;;       browse-url-generic-program "~/src/conkeror/conkeror")
 
 ;; Transparently open compressed files
 (auto-compression-mode t)
 
+;; Enable syntax highlighting for older Emacsen that have it off
+(global-font-lock-mode t)
+
 ;; Save a list of recent files visited.
 (recentf-mode 1)
 
-;; Incremental minibuffer completion
-(icomplete-mode 1)
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
 
 ;; ido-mode is like magic pixie dust!
 (when (> emacs-major-version 21)
   (ido-mode t)
-  (ido-everywhere t)
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t
-        ido-confirm-unique-completion t
-        ido-default-buffer-method 'other-window
         ido-create-new-buffer 'always
-        ido-use-filename-at-point t
+        ido-use-filename-at-point 'guess
         ido-max-prospects 10))
 
+(set-default 'indent-tabs-mode nil)
+(set-default 'indicate-empty-lines t)
 (set-default 'imenu-auto-rescan t)
 
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
@@ -68,24 +78,13 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (random t) ;; Seed the random-number generator
 
-(setq smex-prompt-string "smex ")
-(setq smex-save-file (concat dotfiles-dir "smex.save"))
-
-;; YASnippet
-(yas/initialize)
-(yas/load-directory (concat dotfiles-dir "snippets"))
-
-;; Yeah, Hippie expand
-(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
+;; Hippie expand: at times perhaps too hip
+(delete 'try-expand-line hippie-expand-try-functions-list)
+(delete 'try-expand-list hippie-expand-try-functions-list)
 
 ;; Don't clutter up directories with files~
 (setq backup-directory-alist `(("." . ,(expand-file-name
-                                        (concat dotfiles-dir "backups"))))
-      version-control t
-      kept-new-versions 16
-      kept-old-versions 2
-      delete-old-versions t
-      backup-by-copying-when-linked t)
+                                        (concat dotfiles-dir "backups")))))
 
 ;; nxhtml stuff
 (setq mumamo-chunk-coloring 'submode-colored
@@ -95,9 +94,6 @@
 
 ;; Associate modes with file extensions
 
-(add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode))
-(add-to-list 'auto-mode-alist '("\\.scpt$"        . applescript-mode))
-(add-to-list 'auto-mode-alist '("\\.css$"         . css-mode))
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
@@ -105,26 +101,48 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
 
-;; Keep and trash selected buffers
-(midnight-delay-set 'midnight-delay 4400)
-(add-to-list 'clean-buffer-list-kill-never-buffer-names "*msg*")
-(add-to-list 'clean-buffer-list-kill-never-regexps "^\\*shell-")
-(append clean-buffer-list-kill-buffer-names '("*Shell Command Output*"
-                                              "*Completions*"
-                                              "*Occur*"
-                                              "*Bookmark List*"
-                                              "*Ediff Registry*"
-                                              "*ack*"
-                                              "ri `"
-                                              "*markdown-output*"))
-(add-to-list 'clean-buffer-list-kill-regexps "\\.rb$")
-(add-to-list 'clean-buffer-list-kill-regexps "\\.el$")
+(eval-after-load 'grep
+  '(when (boundp 'grep-find-ignored-files)
+    (add-to-list 'grep-find-ignored-files "target")
+    (add-to-list 'grep-find-ignored-files "*.class")))
 
 ;; Default to unified diffs
 (setq diff-switches "-u")
+
+;; Cosmetics
+
+(set-face-background 'vertical-border "white")
+(set-face-foreground 'vertical-border "white")
+
+(eval-after-load 'diff-mode
+  '(progn
+     (set-face-foreground 'diff-added "green4")
+     (set-face-foreground 'diff-removed "red3")))
+
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")))
+
+(eval-after-load 'mumamo
+  '(eval-after-load 'zenburn
+     '(ignore-errors (set-face-background
+                      'mumamo-background-chunk-submode "gray22"))))
+
+;; Platform-specific stuff
+(when (eq system-type 'darwin)
+  ;; Work around a bug on OS X where system-name is FQDN
+  (setq system-name (car (split-string system-name "\\."))))
+
+;; make emacs use the clipboard
+(setq x-select-enable-clipboard t)
+
+;; Get around the emacswiki spam protection
+(add-hook 'oddmuse-mode-hook
+          (lambda ()
+            (unless (string-match "question" oddmuse-post)
+              (setq oddmuse-post (concat "uihnscuskc=1;" oddmuse-post)))))
 
 (provide 'starter-kit-misc)
 ;;; starter-kit-misc.el ends here
