6d8afe217bdf894d04811eaa16fee1974fd79b93
gary <ge.iams@gmail.com>
2010-06-06 22:43:42 -0400

    restored the starter-kit's js mode setups to master

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -2,29 +2,27 @@
 ;;
 ;; Part of the Emacs Starter Kit
 
-(font-lock-add-keywords
- 'espresso-mode `(("\\(function *\\)("
-                   (0 (progn (compose-region (match-beginning 1) (match-end 1)
-                                             "ƒ")
-                             nil)))))
-
-(font-lock-add-keywords 'espresso-mode
-                        '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
-                           1 font-lock-warning-face t)))
-
 (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 (add-hook 'espresso-mode-hook 'moz-minor-mode)
 (add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
-(add-hook 'espresso-mode-hook 'idle-highlight)
 (setq espresso-indent-level 2)
 
-;; espresso's insert-and-indent doesn't play nicely with pretty-lambda
+;; If you prefer js2-mode, use this instead:
+;; (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+
 (eval-after-load 'espresso
-  '(progn (define-key espresso-mode-map "{" 'paredit-open-brace)
-          (define-key espresso-mode-map "}" 'paredit-close-brace-and-newline)))
+  '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
+          (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
+          ;; fixes problem with pretty function font-lock
+          (define-key espresso-mode-map (kbd ",") 'self-insert-command)
+          (font-lock-add-keywords
+           'espresso-mode `(("\\(function *\\)("
+                             (0 (progn (compose-region (match-beginning 1)
+                                                       (match-end 1) "ƒ")
+                                       nil)))))))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
