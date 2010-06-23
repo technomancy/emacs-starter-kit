71121f12e998ceb2cb4b6c6a7dd6b24556aea96f
Eric Schulte <schulte.eric@gmail.com>
2010-06-23 14:13:50 -0700

    Babel is now part of Org-mode

--- init.el
+++ init.el
@@ -5,27 +5,16 @@
 ;; This is the first thing to get loaded.
 ;;
 
-;; Load up Org Mode and Org Babel for elisp embedded in Org Mode files
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
-(let* ((org-dir (expand-file-name
-		 "lisp" (expand-file-name
-			 "org" (expand-file-name
-				"src" dotfiles-dir))))
-       (org-contrib-dir (expand-file-name
+(add-to-list 'load-path (expand-file-name
                          "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-			  (or load-path nil))))
-  ;; load up Org-mode and Org-babel
-  (require 'org-install)
-  (require 'org-babel-init))
+                                 "org" (expand-file-name
+                                        "src" dotfiles-dir))))
+;; Load up Org Mode and Babel
+(require 'org-install)
 
 ;; load up the main file
 (org-babel-load-file (expand-file-name "starter-kit.org" dotfiles-dir))
 
 ;;; init.el ends here
-(put 'upcase-region 'disabled nil)
-
-(put 'downcase-region 'disabled nil)
