2f7de5cab9aecd46b4c0b2bc2f14f47e6aeb51d8
Phil Hagelberg <technomancy@gmail.com>
2010-11-30 21:01:15 -0800

    Add technomancy and elpa package sources to the list.

--- init.el
+++ init.el
@@ -31,6 +31,9 @@
 (setq custom-file (concat dotfiles-dir "custom.el"))
 
 (require 'package)
+(dolist (source '(("technomancy" . "http://repo.technomancy.us/emacs/")
+                  ("elpa" . "http://tromey.com/elpa/")))
+  (add-to-list 'package-archives source t))
 (package-initialize)
 (require 'starter-kit-elpa)
 
