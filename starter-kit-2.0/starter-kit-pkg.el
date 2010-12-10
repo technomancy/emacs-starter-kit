6bae868d1b81091e2d02e295520bd2617434f717
Phil Hagelberg <technomancy@gmail.com>
2010-12-10 02:00:38 -0800

    Add pkg file.

---
+++ starter-kit-2.0/starter-kit-pkg.el
@@ -0,0 +1,4 @@
+(define-package "Starter Kit" "2.0"
+  "Saner defaults and goodies."
+  '((color-theme "6.5.2") (paredit "22") (idle-highlight "1.0")
+    (magit "0.8.1") (scpaste "0.3") (zenburn "1.0")))
