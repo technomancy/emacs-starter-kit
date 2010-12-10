6ab5cb7fbf9eadd93a04dd32acbafdb9e75b48ed
Phil Hagelberg <technomancy@gmail.com>
2010-12-10 00:17:10 -0800

    Clean up starter-kit-eshell.el.

--- starter-kit-eshell.el
+++ starter-kit-eshell.el
@@ -1,6 +1,41 @@
-;;; starter-kit-eshell.el --- Making the defaults a bit saner
+;;; starter-kit-eshell.el --- Saner defaults and goodies: eshell tweaks
 ;;
-;; Part of the Emacs Starter Kit
+;; Copyright (c) 2008-2010 Phil Hagelberg and contributors
+;;
+;; Author: Phil Hagelberg <technomancy@gmail.com>
+;; URL: http://www.emacswiki.org/cgi-bin/wiki/StarterKit
+;; Version: 2.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; "Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+
+;; This file contains eshell tweaks.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
 
 (setq eshell-cmpl-cycle-completions nil
       eshell-save-history-on-exit t
@@ -13,10 +48,9 @@
      (require 'em-cmpl)
      (setenv "PAGER" "cat")
      (set-face-attribute 'eshell-prompt nil :foreground "turquoise1")
-     (when (< emacs-major-version 23)
-       (add-hook 'eshell-mode-hook ;; for some reason this needs to be a hook
-                 '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-bol)))
-       (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color))
+     (add-hook 'eshell-mode-hook ;; for some reason this needs to be a hook
+               '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-bol)))
+     (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
 
      ;; TODO: submit these via M-x report-emacs-bug
      (add-to-list 'eshell-visual-commands "ssh")
@@ -30,8 +64,8 @@
   "Change directory to the project's root."
   (eshell/cd (locate-dominating-file default-directory "src")))
 
-(defun eshell/find (dir &rest opts)
-  (find-dired dir (mapconcat 'identity opts " ")))
+;; (defun eshell/find (dir &rest opts)
+;;   (find-dired dir (mapconcat 'identity opts " ")))
 
 ;; Port features from
 ;; http://blog.peepcode.com/tutorials/2009/shell-method-missing/shell_method_missing.rb
