803cb62efe7300c882fd8613cc03e521999f1f9d
Phil Hagelberg <technomancy@gmail.com>
2010-12-10 20:15:41 -0800

    Trim down deps of esk2.

--- starter-kit-2.0/starter-kit-pkg.el
+++ starter-kit-2.0/starter-kit-pkg.el
@@ -1,4 +1,3 @@
-(define-package "Starter Kit" "2.0"
+(define-package "starter-kit" "2.0"
   "Saner defaults and goodies."
-  '((color-theme "6.5.2") (paredit "22") (idle-highlight "1.0")
-    (magit "0.8.1") (scpaste "0.3") (zenburn "1.0")))
+  '((paredit "22") (idle-highlight "1.0") (magit "0.8.1") (scpaste "0.3")))
