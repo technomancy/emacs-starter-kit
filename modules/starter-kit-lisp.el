9ae6b3e6c7b64f9b85646d696ee3e2706e6ddacf
Phil Hagelberg <technomancy@gmail.com>
2011-09-13 19:01:44 -0700

    Version 2.0.3 of starter-kit-lisp.

--- modules/starter-kit-lisp.el
+++ modules/starter-kit-lisp.el
@@ -4,9 +4,9 @@
 ;;
 ;; Author: Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/StarterKit
-;; Version: 2.0.2
+;; Version: 2.0.3
 ;; Keywords: convenience
-;; Package-Requires: ((starter-kit "2.0.1") (elisp-slime-nav "0.1"))
+;; Package-Requires: ((starter-kit "2.0.2") (elisp-slime-nav "0.1"))
 
 ;; This file is not part of GNU Emacs.
 
