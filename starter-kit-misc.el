e0b9de2f9e3ace3d832088cb2ac45b0b605cefdd
Phil Hagelberg <technomancy@gmail.com>
2011-09-13 19:01:08 -0700

    Version 2.0.2 of the base starter-kit.

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/StarterKit
-;; Version: 2.0.1
+;; Version: 2.0.2
 ;; Keywords: convenience
 
 ;; This file is not part of GNU Emacs.
