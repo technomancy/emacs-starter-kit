963d97d897b9a2d5dfaa7d74c4f30d6e48313a7c
mjc <github@bitz.ca>
2011-11-09 00:13:48 -0800

    default to using regexp for query-replace

--- modules/starter-kit-bindings.el
+++ modules/starter-kit-bindings.el
@@ -64,6 +64,7 @@
   (global-set-key (kbd "\C-r") 'isearch-backward-regexp)
   (global-set-key (kbd "C-M-s") 'isearch-forward)
   (global-set-key (kbd "C-M-r") 'isearch-backward)
+  (global-set-key (kbd "M-%") 'query-replace-regexp)
 
   ;; Jump to a definition in the current file. (Protip: this is awesome.)
   (global-set-key (kbd "C-x C-i") 'imenu)
