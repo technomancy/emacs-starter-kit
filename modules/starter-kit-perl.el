602af5f5c457828c294ac58e5a7974c95ba1e436
Donald Ephraim Curtis <dcurtis@milkbox.net>
2012-01-23 15:08:37 -0600

    Fix help-command bindings.

--- modules/starter-kit-perl.el
+++ modules/starter-kit-perl.el
@@ -7,7 +7,7 @@
      (define-key cperl-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      (define-key cperl-mode-map (kbd "C-M-h") 'backward-kill-word)))
 
-(global-set-key (kbd "C-h P") 'perldoc)
+(define-key 'help-command "P" 'perldoc)
 
 (add-to-list 'auto-mode-alist '("\\.p[lm]$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
