640e71c96dc709440a0b47e7604469888831b5ee
Phil Hagelberg <technomancy@gmail.com>
2012-02-26 14:53:57 -0800

    No need for user-dir to be on the load-path. Fixes #126.

--- starter-kit.el
+++ starter-kit.el
@@ -51,8 +51,6 @@
         esk-user-config (concat user-emacs-directory user-login-name ".el")
         esk-user-dir (concat user-emacs-directory user-login-name))
 
-  (add-to-list 'load-path esk-user-dir)
-
   (setq smex-save-file (concat user-emacs-directory ".smex-items"))
   (smex-initialize)
   (global-set-key (kbd "M-x") 'smex)
