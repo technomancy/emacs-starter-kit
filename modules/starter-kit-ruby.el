00f31e2202af146a792d6d7a6703447e3a133ca1
Tim McGilchrist <timmcgil@gmail.com>
2012-07-06 15:27:39 +1000

    Latest inf-ruby changes on github have changed inf-ruby-keys to inf-ruby-setup-keybindings

--- modules/starter-kit-ruby.el
+++ modules/starter-kit-ruby.el
@@ -44,7 +44,7 @@
        ;; work around possible elpa bug
        (ignore-errors (require 'ruby-compilation))
        (setq ruby-use-encoding-map nil)
-       (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+       (add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings)
        (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
        (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)))
 
