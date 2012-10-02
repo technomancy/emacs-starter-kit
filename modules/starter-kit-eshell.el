5fe46e54a1b6c480f5767fcba88f1afb22e732fa
Phil Hagelberg <technomancy@gmail.com>
2012-10-01 17:05:35 -0700

    Remove eshell ansi workaround. Fixes #146.
    This was only necessary to work around broken behaviour in 23.

--- modules/starter-kit-eshell.el
+++ modules/starter-kit-eshell.el
@@ -52,7 +52,6 @@
      (set-face-attribute 'eshell-prompt nil :foreground "turquoise1")
      (add-hook 'eshell-mode-hook ;; for some reason this needs to be a hook
                '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-bol)))
-     (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
      (setq eshell-cmpl-cycle-completions nil)
 
      ;; TODO: submit these via M-x report-emacs-bug
