c5710041f877ad9e442665d662eb4514f58e57ce
Jason Lewis <jason@tolkien.lan>
2012-10-29 21:28:51 +1100

    removed bingings for paredit-backward-slurp-sexp M-( it should be in a person's init.el rather than in esk imho

--- modules/starter-kit-lisp.el
+++ modules/starter-kit-lisp.el
@@ -71,12 +71,6 @@
     "Face used to dim parentheses."
     :group 'starter-kit-faces)
 
-  (eval-after-load 'paredit
-    ;; need a binding that works in the terminal
-    '(progn
-       (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
-       (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp)))
-
   (dolist (mode '(scheme emacs-lisp lisp clojure clojurescript))
     (when (> (display-color-cells) 8)
       (font-lock-add-keywords (intern (concat (symbol-name mode) "-mode"))
