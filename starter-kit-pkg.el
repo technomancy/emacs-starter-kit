2f86b5b97a1c49358cc682d3a1e271793f19f9e1
Phil Hagelberg <technomancy@gmail.com>
2013-07-30 21:31:38 -0700

    Remove elisp parts.

--- starter-kit-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "starter-kit" "2.0.3"
-  "Saner defaults and goodies."
-  '((paredit "22") (idle-highlight-mode "1.1.1") (find-file-in-project "3.0")
-    (smex "1.1.2") (ido-ubiquitous "0.3") (magit "0.8.1")))
